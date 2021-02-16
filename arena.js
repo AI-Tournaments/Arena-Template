3b75e690a7e489294a0d3731e2128a3cab4232e5
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2021-02-16 08:56:49 +0100

    Update arena.js

--- arena.js
+++ arena.js
@@ -1,24 +1,8 @@
 'use strict'
 function tick(){
-	/* Ready. Set. Go! */
+	ArenaHelper.postAbort('Did-Not-Start', 'Make a game.');
+	ArenaHelper.postDone();
 }
-function postDone(participants, log){
-	postMessage({type: 'Done', message: {score: participants.getScores(), settings: participants.getSettings(), log: log}});
-}
-function postAbort(participant='', error=''){
-	let participantName = participant.name === undefined ? participant : participant.name;
-	postMessage({type: 'Aborted', message: {participantName: participantName, error: error}})
-}
-onmessage = messageEvent => {
-	importScripts(messageEvent.data.ArenaHelper_url);
-	new ArenaHelper.Participants(messageEvent.data, ()=>{
-		onmessage = messageEvent => {
-			if(messageEvent.data === 'Start'){
-				tick();
-			}
-		}
-		postMessage({type: 'Ready-To-Start', message: null});
-	}, error => {
-		postAbort('Did-Not-Start', error);
-	}, (participantName, error) => postAbort(participantName, error));
+ArenaHelper.init = participants => {
+	tick();
 }
