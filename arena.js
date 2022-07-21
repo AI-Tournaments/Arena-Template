d051122f2068e0f5318d9bababaff9970c9d637c
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2022-07-21 17:35:25 +0200

    Update arena.js
    Signed-off-by: Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>

--- arena.js
+++ arena.js
@@ -1,8 +1,33 @@
 'use strict'
-function tick(){
-	ArenaHelper.postAbort('Did-Not-Start', 'Make a game.'); // Example
-	ArenaHelper.postDone();
+let _participants;
+let _settings;
+function messageParticipants(){
+	let promises = [];
+	_participants.forEach(participant => {
+		let pendingResponse = participant.postMessage({/* TODO: Send participant input. */}).then(response => {
+			participant.payload.response = response;
+		});
+		promises.push(pendingResponse);
+	});
+	Promise.allSettled(promises).then(updateArena);
 }
-ArenaHelper.init = participants => {
-	tick();
+function updateArena(){
+	_participants.forEach(participant => {
+		if(participant.payload.response.message){
+			participant.payload.response.message.data; // TODO: Handle response.
+		}else{
+			// TODO: Handle missing response.
+		}
+	});
+	// TODO: Update arena.
+	ArenaHelper.log('tick', {/* Arena state */});
+	// TODO: Call `ArenaHelper.postDone()` when arena is finished, otherwise call `messageParticipants()` again.
+}
+ArenaHelper.init = (participants, settings) => {
+	_participants = participants;
+	_settings = settings;
+	ArenaHelper.postAbort('Did-Not-Start', 'Invalid settings.'); // Example
+	// TODO: Build arena.
+	ArenaHelper.log('tick', {/* Arena state */});
+	messageParticipants();
 }
